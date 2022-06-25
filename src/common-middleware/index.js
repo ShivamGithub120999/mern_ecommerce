e82bb8dbb4c8c9fe891ca3a1589f7f71013270f0
ShivamGithub120999 <65438973+ShivamGithub120999@users.noreply.github.com>
2022-06-25 13:23:00 +0530

    Adding Categories and Sub Categories at backend

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -1,8 +1,28 @@
-export function requireSignin(req, res, next){
+import jwt from 'jsonwebtoken';
+
+export function requireSignin(req, res, next) {
+  if (req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
-    const user = jwt.verify(token,process.env.JWT_SECRET)
+    const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
-    console.log(token);
-    next();
     //jwt.decode()
+  }else{
+    return res.status(400).json({message: "Authorization required"});
+  }
+  next();
+  
+}
+
+export function userMiddleware(req, res, next) {
+    /* if (req.user.role !== "user") {
+    return res.status(400).json({ message: "User Access denied" });
+  } */
+  next();
+}
+
+export function adminMiddleware(req, res, next) {
+  /* if (req.user.role !== "admin") {
+    return res.status(400).json({ message: "Admin Access denied" });
+  } */
+  next();
 }
--- src/routes/category.js
+++ src/routes/category.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import { addCategory } from ".././controller/category.js"
 import { getCategories } from "../controller/category.js"
+import { requireSignin } from "../common-middleware/index.js"
+import { adminMiddleware } from "../common-middleware/index.js"
 
 const router = express.Router();
 
-router.post('/category/create',addCategory);
+router.post('/category/create',requireSignin,adminMiddleware,addCategory);
 router.get('/category/getcategory',getCategories);
 
 export default router;
